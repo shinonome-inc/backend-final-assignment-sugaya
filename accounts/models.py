cbe494506e269bc9a9f3897debfbe1e74536d4b9
coder-rarara <takkunjunia@icloud.com>
2023-08-17 04:28:01 +0900

    無意味なコメントの削除

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,7 @@
-# from django.contrib.auth.models import AbstractUser
-# from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# class User(AbstractUser):
 class User(AbstractUser):
     email = models.EmailField()
 
