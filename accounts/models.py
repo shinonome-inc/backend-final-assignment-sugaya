e543b37fc87bcc67b842967898d1a63475cb9625
coder-rarara <takkunjunia@icloud.com>
2023-08-17 02:24:41 +0900

    サインアップ機能の実装です。

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 # class User(AbstractUser):
 class User(AbstractUser):
-    email = models.EmailField() 
+    email = models.EmailField()
 
 
 # class FriendShip(models.Model):
