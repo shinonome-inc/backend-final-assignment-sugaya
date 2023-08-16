17bab920364aaf17931b107e4afa7c2089251714
coder-rarara <takkunjunia@icloud.com>
2023-08-17 02:02:46 +0900

    サインアップ機能の実装です。

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 from .forms import SignupForm
 
@@ -14,7 +14,7 @@ class SignupView(CreateView):
     def form_valid(self, form):
         response = super().form_valid(form)
         username = form.cleaned_data["username"]
-        password = form.cleaned_data["password"]
+        password = form.cleaned_data["password1"]
         user = authenticate(self.request, username=username, password=password)
         login(self.request, user)
         return response
